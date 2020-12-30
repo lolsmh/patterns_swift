1283db7da7799e17679af5a900105350aecfd8cd
Даниил Апальков <sugardaddy@MacBook-Pro-Daniil.local>
2020-12-30 21:59:31 +0300

    Added Abstract Factory

---
+++ AbstractFactory/AbstractFactory/Concrete/RapBand/RapProducer.swift
@@ -0,0 +1,14 @@
+//
+//  RapProducer.swift
+//  AbstractFactory
+//
+//  Created by Даниил Апальков on 30.12.2020.
+//
+
+import Foundation
+
+class RapProducer: IProducer {
+    func produceSound() {
+        print("Rap producer going stupid wid da beat")
+    }
+}
---
+++ AbstractFactory/AbstractFactory/Concrete/RapBand/RapVocalist.swift
@@ -0,0 +1,14 @@
+//
+//  RapVocalist.swift
+//  AbstractFactory
+//
+//  Created by Даниил Апальков on 30.12.2020.
+//
+
+import Foundation
+
+class RapVocalist: IVocalist {
+    func singLyrics() {
+        print("Supa illa spitter spits his hella hot bars ayy shit is crazy fasho")
+    }
+}
